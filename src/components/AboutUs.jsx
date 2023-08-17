6033f3f741810ab43de18a37985168f9ada712ba
rizki <muhammadrizkitkj123@gmail.com>
2023-08-17 18:23:53 +0800

    update fetch api dan navbar

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,11 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Restaurant } from "@mui/icons-material";
 import AOS from "aos";
 import 'aos/dist/aos.css';
+import axios from "axios";
+import DOMPurify from "dompurify";
 
 const AboutBody = () => {
+    const [aboutData, setAboutData] = useState([]);
+
     useEffect(() => {
-        AOS.init() 
+        AOS.init();
+    }, []);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/page")
+            .then(res => {
+                if (res.data.data && res.data.data.length > 0) {
+                    const cleanedDescription = DOMPurify.sanitize(res.data.data[0].description);
+                    setAboutData({ description: cleanedDescription });
+                }
+            })
+            .catch(err => console.log(err));
     }, []);
 
     return (
@@ -13,7 +28,7 @@ const AboutBody = () => {
             <div className="flex flex-col-reverse md:flex-row">
                 <div className="md:w-1/2 flex-1 p-2">
                     <div className="flex h-full">
-                    <div className="w-full p-2">
+                        <div className="w-full p-2">
                             <div className="grid grid-cols-2 gap-2 h-96">
                                 <div className="h-full flex items-center justify-center overflow-hidden" data-aos="fade-right" data-aos-duration="1000">
                                     <img className="object-cover " src="/public/assets/img/restoran-1.jpg" alt="Gambar 1" />
@@ -35,8 +50,7 @@ const AboutBody = () => {
                     <div className="col-lg-6">
                         <h5 className="section-title ff-secondary text-left text-xl text-orange-600  font-pacifico">About Us</h5>
                         <h1 className="mb-4 font-nunito text-2xl text-black 2xl:text-4xl">Welcome to <Restaurant className="text-orange-600 pr-1" style={{ height: '58px', width: '40px' }} />Restoran</h1>
-                        <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos erat ipsum et lorem et sit, sed stet lorem sit.</p>
-                        <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                        <p className="mb-4" dangerouslySetInnerHTML={{ __html: aboutData.description }}></p>
                         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
                             <div className="border-start border-5 border-orange-600 border-l-4 p-3 flex items-center">
                                 <h1 className="flex-shrink-0 text-orange-600 text-3xl mb-0 font-nunito" data-toggle="counter-up">2</h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Restaurant } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     const scrollToSection = (sectionId) => {
@@ -9,25 +11,20 @@ const NavBar = () => {
     };
 
     return (
-        <div className="navbar bg-neutral  container 2xl:mx-auto 2xl:px-28 ">
+        <div className="navbar bg-[#0F172B] container 2xl:mx-auto 2xl:px-28 fixed top-0 left-0 w-full z-50 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn  btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral rounded-box w-52">
+                        <li><a onClick={() => scrollToSection("about")}>About Us</a></li>
+                        <li><a onClick={() => scrollToSection("menu")}>Menu</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-orange-600 normal-case text-xl">R&R Burger</a>
+                <Link className=" text-orange-600 normal-case text-xl" to={"/"}>
+                    <h3 className="2xl:text-2xl font-nunito "><Restaurant className="mr-2" style={{ height: '64px', width: '56px' }} />R&R Burger</h3>
+                </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-white">
@@ -36,7 +33,7 @@ const NavBar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <Link to={"/login"} className="">Login</Link>
             </div>
         </div>
     )
